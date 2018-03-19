7757d4739bda3e43c5f133cc7eec29d539b2083e
tjdbsk2017 <31784320+tjdbsk2017@users.noreply.github.com>
2018-03-19 17:34:34 +0900

    Original file upload
    Portfolio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+      auto : true,
+      speed : 800,
+      controls: false,
+//      autoControls: true,
+//      startText: 'Start',
+//      stopText: 'stop',
+//      nextText: 'Next',
+//      prevText: 'Prev',
+      mode: 'fade',
+      infiniteLoop: false,
+      autoHover: false
+  });
+});
